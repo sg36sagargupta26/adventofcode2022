4ae2b5bd137dda632fe1eaa03edef39c8c4193b4
Sagar Gupta <sg36.sagargupta26@gmail.com>
2023-01-25 01:41:20 +0530

    build(part2): day02 solution

--- src/day01/src/lib.rs
+++ src/day01/src/lib.rs
@@ -2,11 +2,6 @@ use std::fs;
 use std::path::PathBuf;
 
 pub fn part1(path: PathBuf) -> u32{
-    if path.exists(){
-        println!("path exists");
-    }else{
-        println!("why")
-    }
     let mut calories = fs::read_to_string(path)
         .unwrap()
         .split("\n\n")
@@ -19,4 +14,19 @@ pub fn part1(path: PathBuf) -> u32{
     calories.sort_by(|a,b|b.cmp(a));
     let ans = calories.first().unwrap_or(&0);
     *ans
+}
+
+pub fn part2(path: PathBuf) -> u32{
+    let mut calories = fs::read_to_string(path)
+        .unwrap()
+        .split("\n\n")
+        .map(|elf_load|{
+            elf_load
+                .lines()
+                .map(|item| item.parse::<u32>().unwrap())
+                .sum::<u32>()
+        }).collect::<Vec<_>>();
+    calories.sort_by(|a,b|b.cmp(a));
+    let sum = calories.iter().take(3).sum::<u32>();
+    sum
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
-use day01::part1;
+use day01::{part1, part2};
 fn main() {
-   let path = PathBuf::from("src/day01/data/inputPart1.txt");
-    println!("{}",part1(path));
+    let path1 = PathBuf::from("src/day01/data/inputPart1.txt");
+    let path2 = PathBuf::from("src/day01/data/inputPart1.txt");
+    println!("{}",part1(path1));
+    println!("{}",part2(path2));
 }
