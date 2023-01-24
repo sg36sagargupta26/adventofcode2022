8ab70bc3cbbab5efa782e0deb51da1347fb8eeec
Sagar Gupta <sg36.sagargupta26@gmail.com>
2023-01-25 01:50:13 +0530

    chore(part2): used clone

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 use day01::{part1, part2};
 fn main() {
     let path1 = PathBuf::from("src/day01/data/inputPart1.txt");
-    let path2 = PathBuf::from("src/day01/data/inputPart1.txt");
+    let path2 = path1.clone();
     println!("{}",part1(path1));
     println!("{}",part2(path2));
 }
