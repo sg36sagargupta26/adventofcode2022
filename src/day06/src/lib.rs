a91c19ca32cfd09f4b5410a173ff713f66a349a1
Sagar Gupta <sg36.sagargupta26@gmail.com>
2023-02-02 23:00:51 +0530

    chore(day6):  removed commented code

--- src/day06/src/lib.rs
+++ src/day06/src/lib.rs
@@ -40,7 +40,6 @@ pub fn part2(path: PathBuf) -> i32{
             if flag{
                 i = i + 1;
                 dequeue.push_back(c);
-                //println!("{:?}",dequeue);
                 if dequeue.len() == 14 {
                     if check_unique(dequeue.clone()){
                         dequeue.pop_front();
@@ -68,23 +67,3 @@ pub fn part2(path: PathBuf) -> i32{
 
      all_unique
  }
-
-// pub fn part2(path: PathBuf) -> u32{
-//     let overlap = fs::read_to_string(path)
-//         .unwrap()
-//         .lines()
-//         .map(|str|{
-//             let elfs = Vec::from_iter(str.split(',').map(String::from));
-//             let elf0=Vec::from_iter(elfs[0].split('-').map(String::from));
-//             let elf1=Vec::from_iter(elfs[1].split('-').map(String::from));
-//             let e00: i32=elf0[0].parse::<i32>().unwrap();
-//             let e01: i32=elf0[1].parse::<i32>().unwrap();
-//             let e10: i32=elf1[0].parse::<i32>().unwrap();
-//             let e11: i32=elf1[1].parse::<i32>().unwrap();
-//             if e00 <= e10 && e01 >= e10 || e00 <= e11 && e01 >= e11 || e10 <= e00 && e11 >= e00 || e10 <= e01 && e11 >= e01{
-//                 return 1;
-//             }
-//             return 0;
-//         }).sum();
-//     overlap
-// }
